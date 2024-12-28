c8ef9e2a44d57b46b4e54bf3a2ea394342cb1ed2
Pascal Van Acker <vanacker.pascal@outlook.com>
2024-12-28 11:33:16 +0100

    Remove unused lines

--- sort_pictures.sh
+++ sort_pictures.sh
@@ -19,8 +19,6 @@ for file in $@; do
             create_date=$(exiftool -b -TrackCreateDate "$file" 2>/dev/null)
         fi
         if [ "$create_date" ] && [ "$create_date" != "$EMPTY_DATE_FORMAT" ] && [ "$create_date" != "-" ]; then
-#           printf "%-34s " "FILE: $file"
-#           printf "%-36s " "CREATE_DATE: $create_date"
             if [ "$file_type" == "JPG" ]; then
                 output=$(exiftool -v '-FileName<DateTimeOriginal' -d %Y/%m/%Y%m%d_%H%M%S%%-c.%%e -overwrite_original "$file" 2>/dev/null)
             else
@@ -32,36 +30,10 @@ for file in $@; do
                 echo -e "\n  ERROR:$output\n"
             else
                 moved=$(echo $moved_line | sed "s/[^']*\('[^']*'\\s-->\\s'[^']*'\).*/\\1/g")
-#               echo "MOVED: $moved"
             fi
         else
-#           printf "%-34s " "FILE: $file"
-#           printf "%-36s " "CREATE_DATE: $create_date"
-#           echo "ERROR: SKIPPED MISSING DATE"
             mkdir -p MISSING
             mv "$file" MISSING/
         fi
     fi
 done
-
-#filepath=$(readlink -f "$1")
-#files=$(find "$filepath" -mindepth 1 -maxdepth 1 -type f)
-
-#for file in $files; do
-#    res=$(exiftool -b -DateTimeOriginal "$file" 2>/dev/null)
-#    if [ "$res" ] && [ "$res" != "$EMPTY_DATE_FORMAT" ] && [ "$res" != "-" ]; then
-#        exiftool '-FileName<DateTimeOriginal'   \
-#           -d %Y/%m/%Y%m%d_%H%M%S%%-c.%%e \
-#           -overwrite_original            \
-#           -a "-ImageDescription=$imagedescripion" "$file" \
-#           -v 2> /dev/null
-#        exit 0
-#    fi
-#
-#    dirname=$(dirname "$file")/missing_tag/
-#    if [ ! -d "$dirname" ]; then
-#        mkdir "$dirname"
-#    fi
-#    mv "$file" "$dirname"
-#
-#done
